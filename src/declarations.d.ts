7ab017562216b242e8f38b574409aa56f91b9066
Aml Abdelrhman <Aml586651@gmail.com>
2026-05-17 20:55:24 +0300

    Fix: suppress ESLint any rule in declarations.d.ts to allow build

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,6 @@
+// قم بإضافة هذا السطر في البداية لإخبار ESLint بتجاهل منع 'any' في هذا الملف
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare module 'swiper/css' {
   const content: any;
   export default content;
